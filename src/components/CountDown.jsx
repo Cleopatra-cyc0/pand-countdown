1909fa6fff6f8a6d3f1f56f39c7bdd9222a9502b
Mees van Dijk <mees@mees.io>
2021-05-31 23:02:01 +0200

    Fix time

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -24,9 +24,7 @@ const CountDown = ({ toDate }) => {
       const hours = Math.floor(
         (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
       )
-      const minutes = Math.floor(
-        (distance % (1000 * 60 * 60)) / (1000 * 60) / 2,
-      )
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
       const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
       setDateDiff({
